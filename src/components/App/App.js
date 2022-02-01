a64b5dced1dc163e75b3b722c3e139dfb940821e
Tou Xiong <touyeexiong@Tous-MacBook-Air.local>
2022-01-31 21:04:11 -0600

    form set up Set up form using hooks.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+      setSubmitting(true);
+
+      setTimeout(() => {
+        setSubmitting(false);
+      }, 3000)
+  }
   return (
     <>
       <div className='wrapper'>
         <h1>How about them apples?</h1>
-        <form>
+        {submitting &&
+          <div>Submitting Form...</div>
+          }
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <label>
             <input name="name"/>
