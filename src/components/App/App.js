50249925781f8526475fd75de827adb5ec2f08b5
Tou Xiong <touyeexiong@gmail.com>
2022-02-03 20:25:04 -0600

    converted uncontrolled components to controlled components.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,14 @@ import React, { useState, useReducer } from 'react';
 import './App.css';
 
 const formReducer = (state, event) => {
+  if(event.reset) {
+    return {
+      apple: '',
+      count: 0,
+      name: '',
+      'gift-wrap': false,
+    }
+  }
   return {
     ...state,
     [event.name]: event.value
@@ -9,7 +17,9 @@ const formReducer = (state, event) => {
 }
 
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, {});
+  const [formData, setFormData] = useReducer(formReducer, {
+    count: 100,
+  });
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = event => {
     event.preventDefault();
@@ -17,6 +27,9 @@ function App() {
 
     setTimeout(() => {
       setSubmitting(false);
+      setFormData({
+        reset: true
+      })
     }, 3000)
   }
 
@@ -46,13 +59,13 @@ function App() {
           <fieldset>
             <label>
               <p>Name</p>
-              <input name="name" onChange={handleChange} />
+              <input name="name" onChange={handleChange} value={formData.name || ''}/>
             </label>
           </fieldset>
           <fieldset>
             <label>
               <p>Name</p>
-              <select name="apple" onChange={handleChange}>
+              <select name="apple" onChange={handleChange} value={formData.apple || ''}>
                 <option value="">--Please choose an option--</option>
                 <option value="fuji">Fuji</option>
                 <option value="Jonathan">Jonathan</option>
@@ -61,11 +74,11 @@ function App() {
             </label>
             <label>
               <p>Count</p>
-              <input type="number" name="count" onChange={handleChange} step="1" />
+              <input type="number" name="count" onChange={handleChange} step="1" value={formData.count || ''}/>
             </label>
             <label>
               <p>Gift Wrap</p>
-              <input type="checkbox" name="gift-wrap" onChange={handleChange} />
+              <input type="checkbox" name="gift-wrap" onChange={handleChange} value={formData['gift-wrap'] || false}/>
             </label>
           </fieldset>
           <button type="submit">Submit</button>
